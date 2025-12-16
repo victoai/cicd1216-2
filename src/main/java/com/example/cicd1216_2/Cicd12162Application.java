a1d087151b4d7f322253df472a9f6bffead66f25
victoai <victoai@naver.com>
2025-12-16 11:25:03 +0900

    cicd2

---
+++ src/main/java/com/example/cicd1216_2/Cicd12162Application.java
@@ -0,0 +1,13 @@
+package com.example.cicd1216_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cicd12162Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cicd12162Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cicd1216_2/Cicd12162ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicd1216_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicd12162ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
